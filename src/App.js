5ffb15d31486a7812002b53cd49425a5f127ef11
Toheeb Opeyemi AJAYI <toheebopeyemi9@gmail.com>
2023-11-19 14:56:53 +0100

    Code Refactor and Layout Added

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import './App.css';
 import Header from './components/Header';
 import SideBar from './components/SideBar';
 import Charts from './components/Charts';
+import Dashboard from './Pages/Dashboard';
 
 
 function App() {
   return (
-    <div >
-        <Header/>
-    
-  <div className=' flex justify-between'>
-        <div>
-            <SideBar/>
-        </div>
-   
-      <div className=' absolute'>
-        <Charts />
-      </div>
-  </div>
-    
-    </div>
+    <Dashboard />
+    // <div >
+    //   <Header />
+
+    //   <div className=' flex justify-between'>
+    //     <div>
+    //       <SideBar />
+    //     </div>
+
+    //     <div className=' absolute'>
+    //       <Charts />
+    //     </div>
+    //   </div>
+
+    // </div>
   );
 }
 
